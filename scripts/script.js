6532199fedeeb4f0d4e58a40acc72a2b7c46a49a
Shadowjian <karakalel.adorable@gmail.com>
2022-11-09 03:00:47 +0800

    4th

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,25 @@
 const form = document.querySelector('#form-app')
 const btnAdd = document.querySelector("#btn-add")
 const input = document.querySelector("#input-app")
-const appsContainer = document.querySelector("#apps-container")
 const nothingHere = document.querySelector("#nothing")
-// nothingHere.classList.remove('hidden')
-    
+const appsContainer = document.querySelector("#apps-container")
+
+
 form.addEventListener('submit', (event)=>{
     event.preventDefault();
     nothingHere.classList.add('hidden')
+    const appsCount = appsContainer.childElementCount + 1
+    if (appsCount < 3) {
+    console.log(appsCount)
+
+    // Create div container
     const appContainer = document.createElement('div')
         appContainer.classList.add("app-container")
         appsContainer.appendChild(appContainer)    
 
+    // Create checkbox 
     const newAppcheckBox = document.createElement('input')
+        newAppcheckBox.title = "Check if done"
         newAppcheckBox.classList.add("done")
         newAppcheckBox.type = "checkbox"
         appContainer.appendChild(newAppcheckBox)    
@@ -29,34 +36,46 @@ form.addEventListener('submit', (event)=>{
             }
         })
 
+    // Create text input
     const newAppText = document.createElement('input')
         newAppText.classList.add('the-app')
         newAppText.value = input.value
         newAppText.readOnly = true
         appContainer.appendChild(newAppText)
     
+    // Create edit button
     const newAppBtnEdit = document.createElement('button')
         newAppBtnEdit.classList.add("btn")
+        newAppBtnEdit.title = "Edit"
         newAppBtnEdit.innerHTML = "<i class='fa-regular fa-pen-to-square'></i>"
         appContainer.appendChild(newAppBtnEdit)
         newAppBtnEdit.addEventListener('click', ()=>{
             newAppBtnEdit.classList.add('hidden')
+            newAppcheckBox.classList.add('hidden')
             newAppBtnDone.classList.remove('hidden')
             newAppText.readOnly = false
+            newAppText.style.color = 'orange' 
+            newAppText.focus()
         })
 
+    // Create save/done button
     const newAppBtnDone = document.createElement('button')
         newAppBtnDone.classList.add("btn")
+        newAppBtnDone.title = "Save edit"
         newAppBtnDone.classList.add("hidden")
         newAppBtnDone.innerHTML = "<i class='fa-regular fa-square-check'></i>"
         appContainer.appendChild(newAppBtnDone)
         newAppBtnDone.addEventListener('click', ()=>{
             newAppBtnDone.classList.add('hidden')
+            newAppcheckBox.classList.remove('hidden')
             newAppBtnEdit.classList.remove('hidden')
+            newAppText.style.color = 'white' 
             newAppText.readOnly = true
         })
 
+    // Create delete button
     const newAppBtnDelete = document.createElement('button')
+        newAppBtnDelete.title = "delete task"
         newAppBtnDelete.classList.add("btn")
         newAppBtnDelete.classList.add("btn-delete")
         newAppBtnDelete.classList.add("hidden")
@@ -65,18 +84,22 @@ form.addEventListener('submit', (event)=>{
         
         newAppBtnDelete.addEventListener('click', ()=>{
             appContainer.remove()
+
+            console.log(appsCount)
         })
 
+    // Reset input value to ""
     input.value = ""
-    const appsCount = appsContainer.childElementCount
-    if (appsCount == 3) {
+    
+    // Set task limit
+        
+    } else {
         btnAdd.disabled=true
         input.disabled=true
-        input.placeholder = "You've reached your max app to build"
+        input.value = "You've reached your max app to build"
         alert("max app to build")
+        input.style.color = "red"
     } 
-
-   
-      
 })
-    
+
+
